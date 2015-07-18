2104265bdae8b6313848c799243ccb5257da45ee
Antoine Hoarau <hoarau.robotics@gmail.com>
2015-07-18 15:37:28 +0200

    add Jacobian dot solver

---
+++ include/rtt_ros_kdl_tools/chainjnttojacdotsolver.hpp
@@ -0,0 +1,71 @@
+/*
+    <one line to give the library's name and an idea of what it does.>
+    Copyright (C) 2015  <copyright holder> <email>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef KDL_CHAINJNTTOJACDOTSOLVER_HPP
+#define KDL_CHAINJNTTOJACDOTSOLVER_HPP
+
+#include "kdl/solveri.hpp"
+#include "kdl/frames.hpp"
+#include "kdl/jntarrayvel.hpp"
+#include "kdl/jacobian.hpp"
+#include "kdl/chain.hpp"
+
+namespace KDL
+{
+
+class ChainJntToJacDotSolver : public SolverI
+{
+
+public:
+    static const int E_JAC_DOT_FAILED= -100;
+  
+    ChainJntToJacDotSolver(const Chain& chain);
+    virtual ~ChainJntToJacDotSolver();
+    virtual int JntToJacDot(const KDL::JntArrayVel& q_in, KDL::Twist& jac_dot_q_dot, int seg_nr = -1);
+    int setLockedJoints(const std::vector<bool> locked_joints);
+
+    /// @copydoc KDL::SolverI::strError()
+    virtual const char* strError(const int error) const;
+    
+    private:
+        const Chain chain;
+        Twist t_tmp;
+        Frame T_tmp;
+        std::vector<bool> locked_joints_;
+        unsigned int nr_of_unlocked_joints_;
+        Frame F_i_im1;
+        Frame F_0_i;
+        std::vector<Rotation> Q;
+        std::vector<Vector> a;
+        std::vector<Rotation> P;
+        std::vector<Vector> e;
+        std::vector<Vector> w;
+        std::vector<Vector> ed;
+        std::vector<Vector> rd;
+        std::vector<Vector> ud;
+        std::vector<Twist> v;
+        std::vector<Vector> r;
+        Twist ud_ed;
+        Twist udi_edi;
+        Twist Ui_v_i1;
+};
+
+}
+#endif
---
+++ src/chainjnttojacdotsolver.cpp
@@ -0,0 +1,180 @@
+/*
+    <one line to give the library's name and an idea of what it does.>
+    Copyright (C) 2015  <copyright holder> <email>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include "rtt_ros_kdl_tools/chainjnttojacdotsolver.hpp"
+
+#include <kdl/kinfam_io.hpp>
+#include <kdl/frames_io.hpp>
+
+namespace KDL
+{
+ChainJntToJacDotSolver::ChainJntToJacDotSolver(const Chain& _chain):
+chain(_chain),locked_joints_(chain.getNrOfJoints(),false),
+nr_of_unlocked_joints_(chain.getNrOfJoints()),
+Q(chain.getNrOfSegments()),
+a(chain.getNrOfSegments()),
+P(chain.getNrOfSegments()),
+e(chain.getNrOfSegments()),
+w(chain.getNrOfSegments()),
+ed(chain.getNrOfSegments()),
+rd(chain.getNrOfSegments()),
+ud(chain.getNrOfSegments()),
+v(chain.getNrOfSegments()),
+r(chain.getNrOfSegments()),
+F_i_im1(Frame::Identity()),
+F_0_i(Frame::Identity())
+{
+
+}
+int ChainJntToJacDotSolver::JntToJacDot(const JntArrayVel& q_in, Twist& jac_dot_q_dot, int seg_nr)
+{
+    unsigned int segmentNr;
+    if(seg_nr<0)
+        segmentNr=chain.getNrOfSegments();
+    else
+        segmentNr = seg_nr;
+
+    if(q_in.q.rows()!=chain.getNrOfJoints())
+        return (error = E_JAC_DOT_FAILED);
+    else if(segmentNr>chain.getNrOfSegments())
+        return (error = E_JAC_DOT_FAILED);
+
+    int j=0;
+    F_0_i = Frame::Identity();
+    
+    j = 0;
+    for (unsigned int i=0;i<segmentNr;++i)
+    {
+      if(chain.getSegment(i).getJoint().getType()!=Joint::None)
+      {
+        F_i_im1 = chain.getSegment(i).pose(q_in.q(j)); //Fk
+        j++;
+      }else{
+        F_i_im1 = chain.getSegment(i).pose(0.0);
+      }
+      F_0_i = F_0_i * F_i_im1;
+      
+      P[i] = F_0_i.M;
+      e[i] = Vector(P[i](0,2) , P[i](1,2), P[i](2,2));
+      
+      Q[i] = F_i_im1.M;
+      a[i] = chain.getSegment(i).getFrameToTip().p;
+    }
+    
+    // Step 1
+    j=0;
+    if(chain.getSegment(0).getJoint().getType()!=Joint::None){
+      w[0] = q_in.qdot(0) * e[0];
+      j++;
+    }else{
+      w[0] = Vector(0,0,0) * e[0];
+    }
+    for (unsigned int i=0;i<segmentNr-1;++i)
+    {
+      if(chain.getSegment(i+1).getJoint().getType()!=Joint::None){
+        w[i+1] = q_in.qdot(j) * e[i] + Q[i].Inverse() * w[i];
+        j++;
+      }else{
+        w[i+1] = Q[i].Inverse() * w[i];
+      }
+    }
+
+    // Step 2
+    for (unsigned int i=0;i<segmentNr;++i)
+    {
+      ed[i] = w[i] * e[i];
+    }
+    
+    // Step 3
+    rd[segmentNr-1] = w[segmentNr-1] * a[segmentNr-1];
+    for (int i=segmentNr-2;i>=0;i--)
+    {
+      rd[i] = w[i] * a[i] + Q[i] * rd[i+1];
+    }
+    
+    r[segmentNr-1] = a[segmentNr-1];
+    for (int i=segmentNr-2;i>=0;i--)
+    {
+      r[i] = a[i] + Q[i] * r[i+1];
+    }
+     
+    ud[0] = e[0] * rd[0];
+    for (unsigned int i=1;i<segmentNr;++i)
+    {
+      ud[i] = ed[i] * r[i] + e[i] * rd[i];
+      
+    }
+
+    // Step 4
+      
+    j = chain.getNrOfJoints() - 1;
+    
+    ud_ed.vel = ud[segmentNr-1];
+    ud_ed.rot = ed[segmentNr-1];
+    
+    if(chain.getSegment(segmentNr-1).getJoint().getType()!=Joint::None){
+      v[segmentNr-1] = q_in.qdot(j) * ud_ed;
+      j--;
+    }else{
+      v[segmentNr-1] = 0.0 * ud_ed;
+    }
+    
+    for (int i=segmentNr-2;i>=0;i--)
+    {
+      Ui_v_i1.vel = Q[i] * v[i+1].vel;
+      Ui_v_i1.rot = Q[i] * v[i+1].rot;
+      udi_edi.vel = ud[i];
+      udi_edi.rot = ed[i];
+            
+      if(chain.getSegment(i).getJoint().getType()!=Joint::None){
+        v[i] = q_in.qdot(j) * udi_edi + Ui_v_i1;
+        j--;
+      }else{
+        v[i] = Ui_v_i1;
+      }
+    }
+ 
+    jac_dot_q_dot = v[0];
+    return (error = E_NOERROR);
+}
+int ChainJntToJacDotSolver::setLockedJoints(const std::vector< bool > locked_joints)
+{
+    if(locked_joints.size()!=locked_joints_.size())
+        return -1;
+    locked_joints_=locked_joints;
+    nr_of_unlocked_joints_=0;
+    for(unsigned int i=0;i<locked_joints_.size();i++){
+        if(!locked_joints_[i])
+            nr_of_unlocked_joints_++;
+    }
+
+    return 0;
+}
+const char* ChainJntToJacDotSolver::strError(const int error) const
+{
+        if (E_JAC_DOT_FAILED == error) return "Jac Dot Failed";
+        else return SolverI::strError(error);
+}
+
+ChainJntToJacDotSolver::~ChainJntToJacDotSolver()
+{
+
+}
+}
