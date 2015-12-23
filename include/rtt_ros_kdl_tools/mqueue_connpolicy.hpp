d21b44fa84fd0d4ba0320b87a3746814f1cba5f2
Antoine Hoarau <hoarau.robotics@gmail.com>
2015-12-23 13:15:30 +0100

    add mqueue over corba connection

---
+++ include/rtt_ros_kdl_tools/mqueue_connpolicy.hpp
@@ -0,0 +1,26 @@
+#ifndef __MQUEUE_CONNPOLICY__
+#define __MQUEUE_CONNPOLICY__
+#include <rtt/transports/mqueue/MQLib.hpp>
+#include <rtt/ConnPolicy.hpp>
+namespace RTT{
+class MQConnPolicy
+{
+    public: static ConnPolicy mq_data(const std::string& name_id="", int data_size = 100, int lock_policy = ConnPolicy::LOCK_FREE, bool init_connection = true, bool pull = false)
+    {
+        ConnPolicy cp = ConnPolicy::data(lock_policy,init_connection,pull);
+        cp.transport = ORO_MQUEUE_PROTOCOL_ID;
+        cp.name_id = name_id;
+        cp.data_size = data_size;
+        return cp;
+    }
+    public: static ConnPolicy mq_buffer(int size, const std::string& name_id="", int data_size = 100, int lock_policy = ConnPolicy::LOCK_FREE, bool init_connection = false, bool pull = false)
+    {
+        ConnPolicy cp = ConnPolicy::buffer(size,lock_policy,init_connection,pull);
+        cp.transport = ORO_MQUEUE_PROTOCOL_ID;
+        cp.name_id = name_id;
+        cp.data_size = data_size;
+        return cp;
+    }
+};
+}
+#endif
