84b721ab5fe0c06726e9d39370d5564270662b2c
Antoine Hoarau <hoarau.robotics@gmail.com>
2016-01-17 12:02:54 +0100

    raised the default buffer size to 1024

--- include/rtt_ros_kdl_tools/mqueue_connpolicy.hpp
+++ include/rtt_ros_kdl_tools/mqueue_connpolicy.hpp
@@ -5,7 +5,7 @@
 namespace RTT{
 class MQConnPolicy
 {
-    public: static ConnPolicy mq_data(const std::string& name_id="", int data_size = 100, int lock_policy = ConnPolicy::LOCK_FREE, bool init_connection = true, bool pull = false)
+    public: static ConnPolicy mq_data(const std::string& name_id="", int data_size = 1024, int lock_policy = ConnPolicy::LOCK_FREE, bool init_connection = true, bool pull = false)
     {
         ConnPolicy cp = ConnPolicy::data(lock_policy,init_connection,pull);
         cp.transport = ORO_MQUEUE_PROTOCOL_ID;
@@ -13,7 +13,7 @@ class MQConnPolicy
         cp.data_size = data_size;
         return cp;
     }
-    public: static ConnPolicy mq_buffer(int size, const std::string& name_id="", int data_size = 100, int lock_policy = ConnPolicy::LOCK_FREE, bool init_connection = false, bool pull = false)
+    public: static ConnPolicy mq_buffer(int size, const std::string& name_id="", int data_size = 1024, int lock_policy = ConnPolicy::LOCK_FREE, bool init_connection = false, bool pull = false)
     {
         ConnPolicy cp = ConnPolicy::buffer(size,lock_policy,init_connection,pull);
         cp.transport = ORO_MQUEUE_PROTOCOL_ID;
